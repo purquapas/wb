353c34e642d59feb5ed291315b7d37b08eef0c25
purquapas <purkuapa2015@mail.ru>
2015-08-09 13:15:13 +0300

    wip

--- upgrade/patch/kmem-11/patch-11x-sys-dev-drm-drmP.h
+++ upgrade/patch/kmem-11/patch-11x-sys-dev-drm-drmP.h
@@ -1,11 +1,11 @@
---- drmP.h.bak	2015-01-13 13:16:06.000000000 +0300
-+++ drmP.h	2015-01-13 13:17:54.000000000 +0300
-@@ -228,7 +228,7 @@
+--- drmP.h-o	2015-08-09 12:57:00.520279000 +0300
++++ drmP.h	2015-08-09 12:57:40.468087000 +0300
+@@ -221,7 +221,7 @@
+ 
  #define PAGE_ALIGN(addr) round_page(addr)
  /* DRM_SUSER returns true if the user is superuser */
- #if __FreeBSD_version >= 700000
 -#define DRM_SUSER(p)		(priv_check(p, PRIV_DRIVER) == 0)
-+#define DRM_SUSER(p)		(priv_check(p, PRIV_DRI_DRIVER) == 0)
- #else
- #define DRM_SUSER(p)		(suser(p) == 0)
- #endif
++#define DRM_SUSER(p)		(priv_check(p, PRIV_DRI_DRIVER) == 0
+ #define DRM_AGP_FIND_DEVICE()	agp_find_device()
+ #define DRM_MTRR_WC		MDF_WRITECOMBINE
+ #define jiffies			ticks
